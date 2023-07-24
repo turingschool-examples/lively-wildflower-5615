a8c1d8ce70cca9fbe31cfba4af4e302b5a38ebeb
Dylan Timmons <dylanstimmons@icloud.com>
2023-07-24 11:46:00 -0600

    Add method for adding rooms

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -2,4 +2,13 @@ class GuestsController < ApplicationController
     def show
         @guest = Guest.find(params[:id])
     end
+
+    def add_room
+        @guest = Guest.find(params[:id])
+        @room = Room.find(params[:room][:room_id])
+
+        @guest.rooms << @room
+
+        redirect_to "/guests/:id"
+    end
 end
