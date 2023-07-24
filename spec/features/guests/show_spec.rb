701f2170907a11037bf6931f0aaa062a8b8e159d
Chris Cullinane <chriscullinane@protonmail.com>
2023-07-24 08:11:19 -0800

    show page complete

---
+++ spec/features/guests/show_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+RSpec.describe "The Guest's Show Page" do
+  before(:each) do
+    @hotel1 = Hotel.create!(name: "Echo Mountain Inn", location: "Echo Mountain")
+    @hotel2 = Hotel.create!(name: "North Face Inn", location: "Echo Mountain")
+    @room1 = Room.create!(hotel: @hotel1, rate: 120, suite: "Sophomore")
+    @room2 = Room.create!(hotel: @hotel1, rate: 100, suite: "Fresnman")
+    @room3 = Room.create!(hotel: @hotel2, rate: 200, suite: "Presdential")
+    @room4 = Room.create!(hotel: @hotel2, rate: 225, suite: "Ambassador")
+    @guest1 = Guest.create!(name: "Charlize Theron", nights: 3)
+    @guest2 = Guest.create!(name: "Winston Churchill", nights: 2)
+    RoomGuest.create!(guest_id: @guest1.id, room_id: @room1.id)
+    RoomGuest.create!(guest_id: @guest2.id, room_id: @room2.id)
+    RoomGuest.create!(guest_id: @guest1.id, room_id: @room3.id)
+    RoomGuest.create!(guest_id: @guest2.id, room_id: @room4.id)
+  end
+
+  it "shows the Guest's name" do
+    visit "/guests/#{@guest1.id}"
+    save_and_open_page
+    expect(page).to have_content("Charlize Theron")
+  end
+end
+
+
+
+# Story 1
+# Guest Show
+
+# As a visitor
+# When I visit a guest's show page
+# I see the guest's name
+# And I see a list of all the rooms they've stayed in
+# including the room's suite, nightly rate, and the name of the hotel that it belongs to.
