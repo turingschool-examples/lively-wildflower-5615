eb51a5c43b5e9e1f57067d1f1814684146ec3977
Derek Chavez <frrenose17@yahoo.com>
2023-07-24 10:58:17 -0600

    feat: add room index view with room info

--- spec/features/rooms/index_spec.rb
+++ spec/features/rooms/index_spec.rb
@@ -12,7 +12,36 @@ RSpec.describe "rooms index page" do
 # and the number of guests that have stayed in that room.
 
   it "displays a list of all rooms" do
+    hotel_1 = Hotel.create!(name: "Aspen Inn", location: "Aspen" )
+    hotel_2 = Hotel.create!(name: "Burger MTN", location: "Durango" )
+    room_1 = hotel_1.rooms.create!(rate: 125, suite: "Presidential")
+    room_2 = hotel_1.rooms.create!(rate: 150, suite: "Honeymoon")
+    room_3 = hotel_2.rooms.create!(rate: 175, suite: "Emperor")
 
+    visit "/rooms"
+    save_and_open_page
+    expect(page).to have_content("Presidential")
+    expect(page).to have_content("Honeymoon")
+    expect(page).to have_content("Emperor")
   end
+
+  it "includes the room's suite, rate and hotel it belongs to" do
+    hotel_1 = Hotel.create!(name: "Aspen Inn", location: "Aspen" )
+    hotel_2 = Hotel.create!(name: "Burger MTN", location: "Durango" )
+    room_1 = hotel_1.rooms.create!(rate: 125, suite: "Presidential")
+    room_2 = hotel_1.rooms.create!(rate: 150, suite: "Honeymoon")
+    room_3 = hotel_2.rooms.create!(rate: 175, suite: "Emperor")
+
+    visit "/rooms"
+
+    expect(page).to have_content("125")
+    expect(page).to have_content("150")
+    expect(page).to have_content("175")
+    expect(page).to have_content("Aspen Inn")
+    expect(page).to have_content("Burger MTN")
+  end
+
+  
+
 
 end
