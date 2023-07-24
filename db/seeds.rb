b06b0c13113d39dadf91f86db814201427a27a01
Allen Russell <garyrusselljr@gmail.com>
2023-07-24 09:44:28 -0600

    test: Add test setup for user story 1, routes, controller for show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# @hotel_1 = Hotel.create!(name: "Hotel California", location: "California")
+# @hotel_2 = Hotel.create!(name: "Hotel Colorado", location: "Colorado")
+
+# @guest_1 = Guest.create!(name: "Jorge", nights: 2)
+# @guest_2 = Guest.create!(name: "Nancy", nights: 3)
+
+# @room_1 = Room.create!(rate: 125, suite: "Economy")
+# @room_2 = Room.create!(rate: 500, suite: "Presidential")
+# @room_3 = Room.create!(rate: 250, suite: "Loft")
+
+
+
+@hotel_1 = Hotel.create!(name: "Hotel California", location: "California")
+
+@guest_1 = Guest.create!(name: "Jorge", nights: 2)
+@guest_2 = Guest.create!(name: "Nancy", nights: 3)
+
+@room_1 = @hotel_1.rooms.create!(rate: 125, suite: "Economy")
+@room_2 = @hotel_1.rooms.create!(rate: 500, suite: "Presidential")
