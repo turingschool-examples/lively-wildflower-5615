91c5784663b338b5fffa297611fed422cc0dd2ff
Chris Cullinane <chriscullinane@protonmail.com>
2023-07-24 08:34:47 -0800

    clean up

--- spec/features/guests/show_spec.rb
+++ spec/features/guests/show_spec.rb
@@ -33,14 +33,3 @@ RSpec.describe "The Guest's Show Page" do
     expect(page).to have_content("Charlize Theron")
   end
 end
-
-
-
-# Story 1
-# Guest Show
-
-# As a visitor
-# When I visit a guest's show page
-# I see the guest's name
-# And I see a list of all the rooms they've stayed in
-# including the room's suite, nightly rate, and the name of the hotel that it belongs to.
