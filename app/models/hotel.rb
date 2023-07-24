0de94252041e7b37ef5669235faf7cf1dd4f3866
Parker Boeing <parker.boeing@gmail.com>
2023-07-24 11:02:40 -0600

    refactor: Add small notes to code

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -3,6 +3,8 @@ class Hotel < ApplicationRecord
   has_many :guests, through: :rooms
 
   def guest_list
+    # I also googled this eleventeen thousand times to ensure it's active record but
+    # TBD...
     guests.distinct
   end
 end
