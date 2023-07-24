9645c0ce7771a26c08fbe07aaa12981ca9369972
Zanna Fitch <zannafitch2004@gmail.com>
2023-07-24 11:02:09 -0500

    feat: add a show page for guests (completed user story 1)

---
+++ spec/features/guests/show_spec.rb
@@ -0,0 +1,51 @@
+require "rails_helper"
+
+RSpec.describe "Show spec" do
+  before(:each) do
+    @emma = Hotel.create!(name: "Hotel Emma", location: "San Antonio")
+    @wolf = Hotel.create!(name: "Great Wolf Lodge", location: "Dallas")
+    
+    @room_1 = @emma.rooms.create!(rate: 500, suite: "Presidential")
+    @room_2 = @emma.rooms.create!(rate: 650, suite: "Penthouse")
+    @room_3 = @wolf.rooms.create!(rate: 300, suite: "Garden")
+    @room_4 = @wolf.rooms.create!(rate: 400, suite: "Animal Kingdom")
+    
+    @sara = Guest.create!(name: "Sara", nights: 2)
+    @leo = Guest.create!(name: "Leo", nights: 3)
+    @piper = Guest.create!(name: "Piper", nights: 3)
+    @paige = Guest.create!(name: "Paige", nights: 1)
+
+    @room_4.guests << @sara
+    @room_1.guests << @sara
+    @room_2.guests << @leo
+    @room_2.guests << @piper
+    @room_2.guests << @paige
+    @room_1.guests << @paige
+  end
+  # user story 1
+  describe "As a visitor, when going to the guest show page" do
+    it "has the guests name and a list of all rooms they are in:shows room's suite, nightly rate, and the name of the hotel that it belongs to" do
+      visit "/guests/#{@paige.id}"
+
+      expect(page).to have_content(@paige.name)
+      expect(page).to have_content(@room_2.rate)
+      expect(page).to have_content(@room_1.rate)
+      expect(page).to have_content(@room_2.suite)
+      expect(page).to have_content(@room_1.suite)
+      expect(page).to have_content(@emma.name)
+      expect(page).to_not have_content(@wolf.name)
+      expect(page).to_not have_content(@piper.name)
+      expect(page).to_not have_content(@room_3.suite)
+    end
+    
+    it "has the guests name and a list of all rooms they are in:shows room's suite, nightly rate, and the name of the hotel that it belongs to" do
+      visit "/guests/#{@sara.id}"
+
+      expect(page).to have_content(@sara.name)
+      expect(page).to have_content(@room_4.suite)
+      expect(page).to have_content(@room_1.suite)
+      expect(page).to have_content(@emma.name)
+      expect(page).to have_content(@wolf.name)
+    end
+  end
+end
