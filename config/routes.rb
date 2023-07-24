82ea3713487bde7861f5c80d60dcdc6fad47eeb3
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-07-24 12:56:59 -0400

    feat: updates routes for add room

--- config/routes.rb
+++ config/routes.rb
@@ -9,23 +9,24 @@ Rails.application.routes.draw do
   # Hotel
   get '/hotels', to: 'hotels#index', as: 'hotels'
   get '/hotels/:id', to: 'hotels#show', as: 'hotel'
-  get '/hotels/edit', to: 'hotels#edit', as: 'edit_hotel'
+  get '/hotels/:id/edit', to: 'hotels#edit', as: 'edit_hotel'
   patch '/hotels/update', to: 'hotels#update', as: 'update_hotel'
   delete '/hotels/delete', to: 'hotels#destroy', as: 'delete_hotel'
 
   # Room
   get '/rooms', to: 'rooms#index', as: 'rooms'
   get '/rooms/:id', to: 'rooms#show', as: 'room'
-  get '/rooms/edit', to: 'rooms#edit', as: 'edit_room'
+  get '/rooms/:id/edit', to: 'rooms#edit', as: 'edit_room'
   patch '/rooms/update', to: 'rooms#update', as: 'update_room'
   delete '/rooms/delete', to: 'rooms#destroy', as: 'delete_room'
 
   # Guest
   get '/guests', to: 'guests#index', as: 'guests'
   get '/guests/:id', to: 'guests#show', as: 'guest'
-  get '/guests/edit', to: 'guests#edit', as: 'edit_guest'
-  patch '/guests/update', to: 'guests#update', as: 'update_guest'
-  delete '/guests/delete', to: 'guests#destroy', as: 'delete_guest'
+  get '/guests/:id/edit', to: 'guests#edit', as: 'edit_guest'
+  patch '/guests/:id', to: 'guests#update', as: 'update_guest'
+  post '/guests/:id/add_room', to: 'guests#add_room', as: 'add_room_guest'
+  delete '/guests/:id', to: 'guests#destroy', as: 'delete_guest'
 
   # Stay
   get '/stays', to: 'stays#index', as: 'stays'
