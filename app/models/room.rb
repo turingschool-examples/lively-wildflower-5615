f3cb965ed318d2b407964fe0300c5d9791b962dc
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-07-24 13:37:33 -0400

    feat: adds distinct_guest_count method

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,4 +2,8 @@ class Room < ApplicationRecord
   belongs_to :hotel
   has_many :stays
   has_many :guests, through: :stays
+
+  def distinct_guest_count
+    stays.distinct.count(:guest_id)
+  end
 end
