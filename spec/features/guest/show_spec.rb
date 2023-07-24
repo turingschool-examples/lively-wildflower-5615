e3b4524f9af1be56d44977dc4362f2a990435200
Dylan Timmons <dylanstimmons@icloud.com>
2023-07-24 11:40:22 -0600

    Add test for user story 2

--- spec/features/guest/show_spec.rb
+++ spec/features/guest/show_spec.rb
@@ -27,4 +27,32 @@ RSpec.describe "Guest Show page" do
       expect(page).to have_content("Hotel: Plaza, Meridian Islands")
     end
   end
+
+  it "displays the form to add a room to the guest" do
+    @guest_1 = Guest.create!(name: "Jake Ryan", nights: 3)
+    @hotel_1 = Hotel.create!(name: "Plaza", location: "Meridian Islands")
+    @room_1 = Room.create!(rate: 130, suite: "Standard", hotel: @hotel_1, guest_id: @guest_1.id)
+
+    visit "/guests/#{@guest_1.id}"
+
+    expect(page).to have_content("Add a Room to this Guest")
+    expect(page).to have_button("Add Room")
+  end
+
+  xit "allows adding of a room to the guest" do
+    @guest_1 = Guest.create!(name: "Jake Ryan", nights: 3)
+    @hotel_1 = Hotel.create!(name: "Plaza", location: "Meridian Islands")
+    @room_1 = Room.create!(rate: 130, suite: "Standard", hotel: @hotel_1, guest_id: @guest_1.id)
+
+    visit "/guests/#{@guest_1.id}"
+
+    within("#add-room-form") do
+      fill_in "Room ID", with: room_2.id
+      click_button "Add Room"
+    end
+
+    expect(page).to have_content("Suite: Deluxe")
+    expect(page).to have_content("Nightly Rate: $200")
+    expect(page).to have_content("Hotel: Plaza, Meridian Islands")
+  end
 end
