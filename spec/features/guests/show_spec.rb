6b67293590c6f377db119b70359f21b4628e9b59
Wil Fady <wrfady@gmail.com>
2023-07-24 13:03:57 -0400

    Feat: complete user story one

--- spec/features/guests/show_spec.rb
+++ spec/features/guests/show_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe "/guests/:id", type: :feature do
     @hotel_1 = Hotel.create!(name: 'Echo Mountain Inn', location: 'Echo Mountain')
     @hotel_2 = Hotel.create!(name: 'Hot Springs Inn', location: 'Hot Springs')
     @hotel_3 = Hotel.create!(name: 'Cherokee Inn', location: 'Cherokee')
-    @room_1 = @hotel_1.rooms.create!(rate: 225, suite: "Presidential")
-    @room_2 = @hotel_2.rooms.create!(rate: 125, suite: "Standard")
-    @room_3 = @hotel_3.rooms.create!(rate: 25, suite: "Closet")
+    @room_1 = @hotel_1.rooms.create!(rate: 250, suite: "Presidential")
+    @room_2 = @hotel_1.rooms.create!(rate: 100, suite: "Standard")
+    @room_3 = @hotel_3.rooms.create!(rate: 30, suite: "Closet")
     @guest_1 = Guest.create!(name: 'Charlize Theron', nights: 3)
     @guest_2 = Guest.create!(name: 'James Blake', nights: 4)
     @guest_3 = Guest.create!(name: 'Dan Smith', nights: 2)
@@ -23,6 +23,18 @@ RSpec.describe "/guests/:id", type: :feature do
         visit "/guests/#{@guest_1.id}"
 
         expect(page).to have_content(@guest_1.name)
+        expect(page).to_not have_content(@guest_3.name)
+      end
+
+      it "I see a list of all the rooms they've stayed in including the room's suite, nightly rate, and the name of the hotel that it belongs to." do
+        visit "/guests/#{@guest_1.id}"
+        save_and_open_page
+        expect(page).to have_content(@room_1.suite)
+        expect(page).to have_content(@room_1.rate)
+        expect(page).to have_content(@hotel_1.name)
+        expect(page).to_not have_content(@room_3.suite)
+        expect(page).to_not have_content(@room_3.rate)
+        expect(page).to_not have_content(@hotel_3.name)
       end
     end
   end
