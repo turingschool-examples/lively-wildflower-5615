383d2d4bcda214199e87a90f17537c51839ff56f
Ethan Black <ethanrossblack@gmail.com>
2023-07-24 11:32:47 -0600

    feat: add relationships to room_guests and guests to hotel model

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,3 +1,7 @@
 class Hotel < ApplicationRecord
   has_many :rooms
+  has_many :room_guests, through: :rooms
+  has_many :guests, through: :room_guests
+
+  
 end
--- spec/models/hotel_spec.rb
+++ spec/models/hotel_spec.rb
@@ -3,5 +3,7 @@ require "rails_helper"
 RSpec.describe Hotel, type: :model do
   describe "relationships" do
     it { should have_many :rooms }
+    it {should have_many(:room_guests).through(:rooms)}
+    it {should have_many(:guests).through(:room_guests)}
   end
 end
