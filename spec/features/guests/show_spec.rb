96eb797138e404eb8465d8acd3e638e4f43fd4a7
Kaina Cockett <kainacockett@gmail.com>
2023-07-24 11:15:30 -0600

    Story 1

--- spec/features/guests/show_spec.rb
+++ spec/features/guests/show_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "Guest Show", type: :feature do
     @hotel_1 = Hotel.create!(name: "Echo Mountain Inn", location: "Echo Mountain")
     @room_1 = Room.create!(rate: 125, suite: "Presidential", hotel: @hotel_1)
     @guest_1 = Guest.create!(name: "Charlize Theron", nights: 3)
-    @guest_1_room_1 = GuestRoom.create!(guest_id: @guest_1, room_id: @room_1)
+    @guest_1_room_1 = GuestRoom.create!(guest_id: @guest_1.id, room_id: @room_1.id)
   end
 
   describe "As a visitor" do
@@ -17,7 +17,7 @@ RSpec.describe "Guest Show", type: :feature do
       end
 
       it "And I see a list of all the rooms they've stayed in including the room's suite, nightly rate, and the name of the hotel that it belongs to." do
-        visit "/guests/#{guest_1.id}"
+        visit "/guests/#{@guest_1.id}"
 
         expect(page).to have_content(@room_1.suite)
         expect(page).to have_content(@room_1.rate)
