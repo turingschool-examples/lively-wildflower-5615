ec762cc79b003a56d6a1b029711bbe24326718a8
Artemy Gibson <artemygibson@gmail.com>
2023-07-24 08:54:24 -0700

    Add guestrooms controller

---
+++ app/controllers/guest_rooms_controller.rb
@@ -0,0 +1,10 @@
+class GuestRoomsController < ApplicationController
+  def create
+    GuestRoom.create!(
+      guest_id: params[:id],
+      room_id: params[:room_id]
+    )
+
+    redirect_to "/guests/#{params[:id]}"
+  end
+end
