b1a15b1bdc65dee7e9b28fe65c10f82d780dfedd
Ethan Black <ethanrossblack@gmail.com>
2023-07-24 09:56:42 -0600

    feat: add hotel_name instance method to room model

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,4 +2,8 @@ class Room < ApplicationRecord
   belongs_to :hotel
   has_many :room_guests
   has_many :guests, through: :room_guests
+
+  def hotel_name
+    hotel.name
+  end
 end
--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -6,4 +6,19 @@ RSpec.describe Room, type: :model do
     it {should have_many :room_guests}
     it {should have_many(:guests).through(:room_guests)}
   end
+
+  before(:each) do
+    @hotel1 = Hotel.create!(name:"Vail Inn", location:"Vail")
+    @hotel2 = Hotel.create!(name:"Lionshead Chalet", location:"Lionshead")
+
+    @room1 = @hotel1.rooms.create!(rate: 125, suite: "Presidential")
+    @room2 = @hotel2.rooms.create!(rate: 130, suite: "Executive")
+  end
+
+  describe "instance methods" do
+    it "#hotel_name" do
+      expect(@room1.hotel_name).to eq("Vail Inn")
+      expect(@room2.hotel_name).to eq("Lionshead Chalet")
+    end
+  end
 end
