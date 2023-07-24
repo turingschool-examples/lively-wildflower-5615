2d567b130f42113d6093461c69dea30a14b1e2d3
Allen Russell <garyrusselljr@gmail.com>
2023-07-24 10:12:30 -0600

    feat: Complete user story 1 show page, view, controller show method

--- spec/features/guests/show_spec.rb
+++ spec/features/guests/show_spec.rb
@@ -5,18 +5,25 @@ RSpec.describe "guests show page" do
     @hotel_1 = Hotel.create!(name: "Hotel California", location: "California")
 
     @guest_1 = Guest.create!(name: "Jorge", nights: 2)
-    @guest_2 = Guest.create!(name: "Nancy", nights: 3)
 
     @room_1 = @hotel_1.rooms.create!(rate: 125, suite: "Economy")
     @room_2 = @hotel_1.rooms.create!(rate: 500, suite: "Presidential")
+
+    GuestRoom.create!(guest: @guest_1, room: @room_1)
+    GuestRoom.create!(guest: @guest_1, room: @room_2)
   end
 
   describe "as a visitor" do
     describe "when I visit a guest's show page" do
-      it "I see the guest's name, I see a list of all the rooms they've stayin in including the room's suite, nightly rate, and the name of the hotel that it belongs to" do
+      it "I see the guest's name, I see a list of all the rooms they've stayed in including the room's suite, nightly rate, and the name of the hotel that it belongs to" do
         visit "/guests/#{@guest_1.id}"
 
-        
+        expect(page).to have_content(@guest_1.name)
+        expect(page).to have_content(@hotel_1.name)
+        expect(page).to have_content(@room_1.rate)
+        expect(page).to have_content(@room_1.suite)
+        expect(page).to have_content(@room_2.rate)
+        expect(page).to have_content(@room_2.suite)
       end
     end
   end
