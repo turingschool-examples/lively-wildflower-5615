2ff760365a450bb616e61153cd93e41932df4db1
Connor Richmond <richmond.connor@gmail.com>
2023-07-24 13:02:03 -0400

    User Story 2

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -2,4 +2,14 @@ class GuestsController < ApplicationController
   def show
     @guest = Guest.find(params[:id])
   end
+
+  def add_room
+    @guest = Guest.find(params[:id])
+    room_id = params[:room_id]
+
+    room = Room.find_by(id: room_id)
+    @guest.rooms << room if room
+
+    redirect_to "/guests/#{params[:id]}"
+  end
 end
--- spec/features/guests/show_spec.rb
+++ spec/features/guests/show_spec.rb
@@ -7,11 +7,12 @@ RSpec.describe 'guests', type: :feature do
     before(:each) do
       @hotel = Hotel.create(name: 'Green Mountian Inn', location: 'Stowe')
       @room1 = @hotel.rooms.create(rate: 200, suite: 'Super')
+      @room2 = @hotel.rooms.create(rate: 150, suite: 'Deluxe')
       @guest1 = Guest.create(name: 'Kye', nights: 6)
       @guests_room = GuestsRoom.create(guest: @guest1, room: @room1)
     end
 
-    it 'shows guests with information on show page' do
+    it "shows guests with information on show page" do
       visit "/guests/#{@guest1.id}"
 
       expect(page).to have_content(@guest1.name)
@@ -20,6 +21,15 @@ RSpec.describe 'guests', type: :feature do
       expect(page).to have_content("Nightly Rate: $#{@room1.rate}")
     end
 
-  end
+    it "can add rooms to guest" do
+      visit "/guests/#{@guest1.id}"
 
+      fill_in "Room ID:", with: @room2.id
+      click_button "Add Room"
+
+      expect(page).to have_content(@room2.hotel.name)
+      expect(page).to have_content(@room2.suite)
+      expect(page).to have_content("$#{@room2.rate}")
+    end
+  end
 end
