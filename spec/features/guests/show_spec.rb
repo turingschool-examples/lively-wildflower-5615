c438de092c0cda8e54ea85fe9509d9bcfe8740aa
Ethan Black <ethanrossblack@gmail.com>
2023-07-24 10:04:35 -0600

    feat: hotel show page shows room history with room details

--- spec/features/guests/show_spec.rb
+++ spec/features/guests/show_spec.rb
@@ -24,24 +24,73 @@ RSpec.describe "The Guest Show Page", type: :feature do
   it "I see a guest's name" do
     visit "/guests/#{@guest1.id}"
     
-    within("div#guest-info") do
+    within("div#guest_info") do
       expect(page).to have_content(@guest1.name)
       expect(page).to_not have_content(@guest2.name)
     end
     
     visit "/guests/#{@guest2.id}"
     
-    within("div#guest-info") do
+    within("div#guest_info") do
       expect(page).to have_content(@guest2.name)
       expect(page).to_not have_content(@guest1.name)
     end
   end
 
-  xit "I see a list of all the rooms they've stayed in" do
+  it "I see a list of all the rooms they've stayed in, including information on a room's suite, nightly rate, and the name of the hotel to which it belongs" do
+    # Guest 1 Room History
+    visit "/guests/#{@guest1.id}"
+    within("div#guest_room_history") do
+      expect(page).to have_css("div#room-#{@room1.id}")
+      within("div#room-#{@room1.id}") do
+        expect(page).to have_content(@room1.suite)
+        expect(page).to have_content(@room1.rate)
+        expect(page).to have_content(@hotel.name)
+      end
 
-  end
+      expect(page).to have_css("div#room-#{@room2.id}")
+      within("div#room-#{@room2.id}") do
+        expect(page).to have_content(@room2.suite)
+        expect(page).to have_content(@room2.rate)
+        expect(page).to have_content(@hotel.name)
+      end
 
-  xit "Each listed room includes the room's suite, nightly rate, and the name of the hotel to which it belongs" do
+      expect(page).to have_css("div#room-#{@room3.id}")
+      within("div#room-#{@room3.id}") do
+        expect(page).to have_content(@room3.suite)
+        expect(page).to have_content(@room3.rate)
+        expect(page).to have_content(@hotel.name)
+      end
+    end
+    
+    #Guest 2 Room History
+    visit "/guests/#{@guest2.id}"
+    within("div#guest_room_history") do
+      expect(page).to have_css("div#room-#{@room1.id}")
+      within("div#room-#{@room1.id}") do
+        expect(page).to have_content(@room1.suite)
+        expect(page).to have_content(@room1.rate)
+        expect(page).to have_content(@hotel.name)
+      end
+      
+      expect(page).to_not have_css("div#room-#{@room2.id}")
+      
+      expect(page).to_not have_css("div#room-#{@room3.id}")
+    end
 
+    # Guest 3 Room History
+    visit "/guests/#{@guest3.id}"
+    within("div#guest_room_history") do
+      expect(page).to_not have_css("div#room-#{@room1.id}")
+    
+      expect(page).to have_css("div#room-#{@room2.id}")
+      within("div#room-#{@room2.id}") do
+        expect(page).to have_content(@room2.suite)
+        expect(page).to have_content(@room2.rate)
+        expect(page).to have_content(@hotel.name)
+      end
+    
+      expect(page).to_not have_css("div#room-#{@room3.id}")
+    end
   end
 end
