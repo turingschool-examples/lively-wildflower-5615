9baf69358eeb2d3bf66f5f66583c9320834906b8
Dylan Timmons <dylanstimmons@icloud.com>
2023-07-24 10:51:45 -0600

    final fix to make passing test

--- spec/features/guest/show_spec.rb
+++ spec/features/guest/show_spec.rb
@@ -3,17 +3,12 @@ require 'rails_helper'
 RSpec.describe "Guest Show page" do
   it "displays the guest's name and their rooms' information" do
 
-    guest_1 = Guest.create!(name: "Jake Ryan", nights: 3)
-    hotel_1 = Hotel.create!(name: "Plaza", location: "Meridian Islands")
-    room_1 = Room.create!(rate: 130, suite: "Standard", hotel: hotel_1)
-    room_2 = Room.create!(rate: 500, suite: "Presidential", hotel: hotel_1)
+    @guest_1 = Guest.create!(name: "Jake Ryan", nights: 3)
+    @hotel_1 = Hotel.create!(name: "Plaza", location: "Meridian Islands")
+    @room_1 = Room.create!(rate: 130, suite: "Standard", hotel: @hotel_1, guest_id: @guest_1.id)
+    @room_2 = Room.create!(rate: 500, suite: "Presidential", hotel: @hotel_1, guest_id: @guest_1.id)
 
-
-    room_1.guests << guest_1
-    room_2.guests << guest_1
-
-
-    visit "/guests/#{guest_1.id}"
+    visit "/guests/#{@guest_1.id}"
 
 
     expect(page).to have_content("Guest Name: Jake Ryan")
