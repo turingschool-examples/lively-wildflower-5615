0d69d4e63642df0339e393a5ffe3964765d9bf35
Derek Chavez <frrenose17@yahoo.com>
2023-07-24 10:05:54 -0600

    feat: add the list of rooms visited to the guests show page

--- spec/features/guests/show_spec.rb
+++ spec/features/guests/show_spec.rb
@@ -13,7 +13,8 @@ RSpec.describe "guests show page" do
 
   it "displays the guest's name" do
     hotel_1 = Hotel.create!(name: "Aspen Inn", location: "Aspen" )
-    room_1 = Room.create!(rate: 125, suite: "Presidential")
+    room_1 = hotel_1.rooms.create!(rate: 125, suite: "Presidential")
+
     guest_1 = Guest.create!(name: "Charlie Day", nights: 3)
 
     visit "/guests/#{guest_1.id}"
@@ -23,16 +24,21 @@ RSpec.describe "guests show page" do
 
   it "has a list of all the rooms they've stayed in with nightly rate, suite and hotel affiliation" do
     hotel_1 = Hotel.create!(name: "Aspen Inn", location: "Aspen" )
-    room_1 = Room.create!(rate: 125, suite: "Presidential")
-    room_2 = Room.create!(rate: 150, suite: "Honeymoon")
+    room_1 = hotel_1.rooms.create!(rate: 125, suite: "Presidential")
+    room_2 = hotel_1.rooms.create!(rate: 150, suite: "Honeymoon")
+
     guest_1 = Guest.create!(name: "Charlie Day", nights: 3)
+
+    guest_1.rooms << room_1
+    guest_1.rooms << room_2
 
     visit "/guests/#{guest_1.id}"
 
-    expect(page).to have_content("Suite: Presidential")
-    expect(page).to have_content("Rate: 125")
-    expect(page).to have_content("Suite: Honeymoon")
-    expect(page).to have_content("Rate: 150")
+
+    expect(page).to have_content("Presidential")
+    expect(page).to have_content("125")
+    expect(page).to have_content("Honeymoon")
+    expect(page).to have_content("150")
     expect(page).to have_content("Aspen Inn")
   end
 end
